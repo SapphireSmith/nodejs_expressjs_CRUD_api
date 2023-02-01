2550d4d8be85245b2ccf6f241fae81f682d31485
Sapphire Smith <ssapphire527@gmail.com>
2023-02-01 13:55:01 +0530

    First release of CRUD api

--- controllers/users.js
+++ controllers/users.js
@@ -1,36 +1,45 @@
 import { v4 as uuid } from 'uuid';
 
+
 let users = [];
 
 export const getUsers = (req, res) => {
-    console.log(`Users in the database: ${users}`);
-
     res.send(users);
 }
 
-export const createUser = (req, res) => {   
+export const createUser = (req, res) => {
     const user = req.body;
-
-    users.push({...user, id: uuid()});
-    
-    console.log(`User [${user.username}] added to the database.`);
+    console.log(user);
+    users.push({ id: uuid(), ...user });
+    console.log(`User ${user.firstName} added to the database.`);
+    res.end();
 };
 
 export const getUser = (req, res) => {
-    res.send(req.params.id)
+    const { id } = req.params;
+    console.log(id);
+    const user = users.find((user) => user.id === id)
+    console.log(user);
+    res.send(user);
 };
 
-export const deleteUser = (req, res) => { 
-    console.log(`user with id ${req.params.id} has been deleted`);
-    
+export const deleteUser = (req, res) => {
     users = users.filter((user) => user.id !== req.params.id);
+    console.log(`user with id ${req.params.id} has been deleted`);
+    res.end()
 };
 
-export const updateUser =  (req,res) => {
+export const updateUser = (req, res) => {
     const user = users.find((user) => user.id === req.params.id);
-    
-    user.username = req.body.username;
-    user.age = req.body.age;
-
-    console.log(`username has been updated to ${req.body.username}.age has been updated to ${req.body.age}`)
+    if (req.body.firstName) {
+        user.firstName = req.body.firstName;
+    }
+    if (req.body.lastName) {
+        user.lastName = req.body.lastName;
+    }
+    if (req.body.age) {
+        user.age = req.body.age;
+    }
+    console.log(`username has been updated to ${req.body.username}.age has been updated to ${req.body.age}`);
+    res.end()
 };
--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import bodyParser from "body-parser";
 import usersRoutes from "./routes/users.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
 app.use("/people", usersRoutes);
-app.get("/", (req, res) => res.send("Welcome to the Users API!"));
-app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+app.get("/", (req, res) => res.send("Welcome to the CRUD API"));
+app.all("*", (req, res) =>res.send(" Sorry.. You've tried reaching a route that doesn't exist."));
 
 app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
